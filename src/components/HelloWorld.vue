34ae0156842d84588dc995212a471aa950bb539e
曹少帅 <caoshaoshuai@xiaomi.com>
2021-11-25 21:14:45 +0800

    feat: 项目初始化,添加了vitepress,剩余添加husky等工具包

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+// defineProps<{ msg: string }>();
+console.log(123465)
+
+const count = ref(0);
+
+
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style>
+</style>
